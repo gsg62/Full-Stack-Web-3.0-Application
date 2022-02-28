c9be6c90c11070a4d09ef1ca32618ba0ae0fb5ce
Greg Geary <gsg62@nau.edu>
2022-02-28 16:53:53 -0700

    navbar and welcome page created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Full-Stack-Web-3.0-Application
+
+This is my first Web 3.0 Full-stack application built in collaboration with braydensalmins. This project utilizes React.js, Solidity, and Metamask to provide a clean, basic user interface that interacts with a decentralized backend.
---
+++ client/krypt/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Navbar, Welcome, Footer, Services, Transactions } from './components';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+         <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
